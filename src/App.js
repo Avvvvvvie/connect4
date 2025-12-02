3e6898f45d95c05a0e8c1c14d48dea1a8ea515ab
Annie Hemprich <hemprann@students.zhaw.ch>
2025-12-02 21:01:30 +0100

    Add basic components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
 
+let board = new Board()
+
+function LoadButton() {
+    const handler = () => {
+        let data = JSON.parse(localStorage.getItem('board'))
+        board = new Board(data)
+    }
+    return (
+        <button onClick={handler}>
+            Load Game
+        </button>
+    )
+}
+function SaveButton() {
+  const handler = () => {
+      let data = JSON.stringify(board.save())
+      localStorage.setItem('board', data)
+  }
+  return (
+      <button onClick={handler}>
+        Save Game
+      </button>
+  )
+}
+
+function WinDisplay() {
+    return (
+        <div className="winDisplay">
+
+        </div>
+    )
+}
+
+function TurnDisplay() {
+    return (
+        <div className="turnDisplay">
+
+        </div>
+    )
+}
+
+function Column(props) {
+    return (
+        <div className="column" onClick={handler}>
+            {
+                props.children
+            }
+        </div>
+    )
+}
+
+function Field({ value }) {
+    return (
+        <div className="field">
+            <div className="piece" data-state={ value }></div>
+        </div>
+    )
+}
+
+function Board() {
+    return (
+        <div className="board">
+            {
+                board.state.forEach((column, index) => {
+                    function handler() {
+                        board.play(index)
+                    }
+                    return (
+                        <Column onClick={handler}>
+                            {
+                                column.forEach((value) => {
+                                    return (
+                                        <Field value={value}/>
+                                    )
+                                })
+                            }
+                        </Column>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +102,10 @@ function App() {
           Learn React
         </a>
       </header>
+        <LoadButton/><SaveButton/>
+        <Board/>
+        <TurnDisplay/>
+        <WinDisplay/>
     </div>
   );
 }
